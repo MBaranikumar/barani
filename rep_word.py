1fb48803dde4fd872c5a3179eb6eb084084c0f54
Barani Kumar <baranikumar.mathiazahagan@kone.com>
2020-05-18 18:54:35 +0530

    new testcase written

--- rep_word.py
+++ rep_word.py
@@ -1,17 +1,22 @@
-user_input = input("enter your paragraph: ")
-#print (user_input)
 
-find_string = input("enter find string: ")
+def rep_word():
+    user_input = "Hi hello welcome, this is bigboss. cha chicku chicku chim bigboss"
+    #print (user_input)
 
-split_string = user_input.split()
-count = 0
+    #find_string = input("enter find string: ")
+    find_string = "bigboss"
 
-for i in split_string:
-    if find_string in i:
-        count+=1
+    split_string = user_input.split()
+    count = 0
+
+    for i in split_string:
+        if find_string in i:
+            count+=1
+        else:
+            pass
+
+    if count == 0:
+        print ("match not found")
     else:
-        pass
-if count == 0:
-    print ("match not found")
-else:
-    print (count)
+        print (count)
+rep_word()
